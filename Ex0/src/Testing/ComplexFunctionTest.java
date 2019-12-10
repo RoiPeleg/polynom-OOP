6720361528f150613fcf1a76b9ccb5bce17162fa
RoiPeleg <37403170+RoiPeleg@users.noreply.github.com>
2019-12-10 21:53:59 +0200

    added test file for cf

---
+++ Ex0/src/Testing/ComplexFunctionTest.java
@@ -0,0 +1,57 @@
+package Testing;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ComplexFunctionTest {
+
+    @Test
+    public void area() {
+    }
+
+    @Test
+    public void root() {
+
+    }
+
+    @Test
+    public void f() {
+    }
+
+    @Test
+    public void initFromString() {
+    }
+
+    @Test
+    public void copy() {
+    }
+
+    @Test
+    public void max() {
+    }
+
+    @Test
+    public void min() {
+    }
+
+    @Test
+    public void plus() {
+    }
+
+    @Test
+    public void mul() {
+    }
+
+    @Test
+    public void div() {
+    }
+
+    @Test
+    public void comp() {
+    }
+
+    @Test
+    public void testEquals() {
+    }
+}
